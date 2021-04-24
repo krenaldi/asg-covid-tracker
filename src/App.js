b26cee041fb5a6ff2affa20545c46ae073698cb3
Kris Renaldi <krenaldi@comcast.net>
2021-04-24 18:24:01 -0400

    Added API to fetch data from CDC Covid Tracker API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-// import Cards from './components/Cards/Cards'
-// import Chart from './components/Chart/Chart'
-// import CountryPicker from './components/CountryPicker/CountryPicker'
-
+import { useEffect, useState } from 'react'
 import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css'
+import { fetchData } from './API'
 
 function App() {
+  const [ data, setData ] = useState({});
+  
+  useEffect(async () => {
+    const fetchedData = await fetchData();
+    console.log(fetchedData);
+    setData(fetchedData)
+  }, [])
+
+
   return (
     <div className={styles.container}>
       <Cards />
