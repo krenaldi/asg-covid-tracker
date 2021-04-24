7ee35840473e14b8a04012df0d4f1f91b9f7a524
Kris Renaldi <krenaldi@comcast.net>
2021-04-24 16:23:36 -0400

    Created Cards, Chart, & CountryPicker components and added to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+// import Cards from './components/Cards/Cards'
+// import Chart from './components/Chart/Chart'
+// import CountryPicker from './components/CountryPicker/CountryPicker'
+
+import { Cards, Chart, CountryPicker } from './components'
 
 function App() {
   return (
     <div >
-      
+      <Cards />
+      <Chart />
+      <CountryPicker />
     </div>
   );
 }
