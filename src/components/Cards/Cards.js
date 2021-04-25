cc84d80c018f1924a762e369868ce3d964ee5cca
Kris Renaldi <krenaldi@comcast.net>
2021-04-24 21:14:01 -0400

    Added style to Data Cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,6 +1,7 @@
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import styles from './Cards.module.css';
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
     // console.log(confirmed)
@@ -10,7 +11,7 @@ const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>
                             Infected
@@ -27,7 +28,7 @@ const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
                         <Typography variant='body2'>Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>
                             Recovered
@@ -44,7 +45,7 @@ const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
                         <Typography variant='body2'>Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>
                             Deaths
