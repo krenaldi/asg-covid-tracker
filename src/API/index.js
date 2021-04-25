df7e4bbeca5214facf1a4c2912dae8d61f07ebfe
Kris Renaldi <krenaldi@comcast.net>
2021-04-24 21:48:34 -0400

    Added chart.js package to display charts & retrieve data from dailySummary endpoint - https://covid19.mathdro.id/api/daily

--- src/API/index.js
+++ src/API/index.js
@@ -5,9 +5,18 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
     try {
         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
-        
+
         return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
         console.log(error);
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        const response = await axios.get(`${url}/daily`);
+        console.log(response);
+    } catch (error) {
+        console.log(error)
     }
 }
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import { fetchDailyData } from '../../API';
+import { Line, Bar } from 'react-chartjs-2';
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        console.log(dailyData);
+        fetchAPI();
+    }, [])
+
+    const lineChart = (
+        dailyData[0]
+            ? (
+                <Line
+                    data={{
+                        labels: '',
+                        datasets: [{}, {}],
+                    }}
+                />
+            ) : null
+    )
+
     return (
         <div>
             <h1>Chart</h1>
